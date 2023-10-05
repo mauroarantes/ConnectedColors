67a29d2141b200f0710c78942dd6b46af645ed26
Mauro Arantes <mauro.arantes@gmail.com>
2023-10-05 10:35:29 +0100

    Initial Commit

---
+++ ConnectedColors/ConnectedColorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectedColorsApp.swift
+//  ConnectedColors
+//
+//  Created by Mauro Arantes on 05/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct ConnectedColorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConnectedColors/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ConnectedColors
+//
+//  Created by Mauro Arantes on 05/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
