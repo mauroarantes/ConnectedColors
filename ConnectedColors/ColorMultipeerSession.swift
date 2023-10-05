359b7bdcda8da13ce0f2df56db0cb07fadb65c97
Mauro Arantes <mauro.arantes@gmail.com>
2023-10-05 10:41:25 +0100

    Added ColorMultiSession.swift

---
+++ ConnectedColors/ColorMultipeerSession.swift
@@ -0,0 +1,84 @@
+//
+//  ColorMultipeerSession.swift
+//  ConnectedColors
+//
+//  Created by Mauro Arantes on 05/10/2023.
+//
+
+import MultipeerConnectivity
+import os
+
+class ColorMultipeerSession: NSObject, ObservableObject {
+    private let serviceType = "example-color"
+private let myPeerId = MCPeerID(displayName: UIDevice.current.name)
+private let serviceAdvertiser: MCNearbyServiceAdvertiser
+private let serviceBrowser: MCNearbyServiceBrowser
+private let session: MCSession
+    private let log = Logger()
+
+    override init() {
+        session = MCSession(peer: myPeerId, securityIdentity: nil, encryptionPreference: .none)
+        serviceAdvertiser = MCNearbyServiceAdvertiser(peer: myPeerId, discoveryInfo: nil, serviceType: serviceType)
+        serviceBrowser = MCNearbyServiceBrowser(peer: myPeerId, serviceType: serviceType)
+
+        super.init()
+
+        session.delegate = self
+        serviceAdvertiser.delegate = self
+        serviceBrowser.delegate = self
+
+        serviceAdvertiser.startAdvertisingPeer()
+        serviceBrowser.startBrowsingForPeers()
+    }
+
+    deinit {
+        serviceAdvertiser.stopAdvertisingPeer()
+        serviceBrowser.stopBrowsingForPeers()
+    }
+}
+
+extension ColorMultipeerSession: MCNearbyServiceAdvertiserDelegate {
+    func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didNotStartAdvertisingPeer error: Error) {
+        log.error("ServiceAdvertiser didNotStartAdvertisingPeer: \(String(describing: error))")
+    }
+
+    func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didReceiveInvitationFromPeer peerID: MCPeerID, withContext context: Data?, invitationHandler: @escaping (Bool, MCSession?) -> Void) {
+        log.info("didReceiveInvitationFromPeer \(peerID)")
+    }
+}
+
+extension ColorMultipeerSession: MCNearbyServiceBrowserDelegate {
+    func browser(_ browser: MCNearbyServiceBrowser, didNotStartBrowsingForPeers error: Error) {
+        log.error("ServiceBrowser didNotStartBrowsingForPeers: \(String(describing: error))")
+    }
+
+    func browser(_ browser: MCNearbyServiceBrowser, foundPeer peerID: MCPeerID, withDiscoveryInfo info: [String: String]?) {
+        log.info("ServiceBrowser found peer: \(peerID)")
+    }
+
+    func browser(_ browser: MCNearbyServiceBrowser, lostPeer peerID: MCPeerID) {
+        log.info("ServiceBrowser lost peer: \(peerID)")
+    }
+}
+
+extension ColorMultipeerSession: MCSessionDelegate {
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+        log.info("peer \(peerID) didChangeState: \(state.rawValue)")
+    }
+
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+        log.info("didReceive bytes \(data.count) bytes")
+    }
+
+    public func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+        log.error("Receiving streams is not supported")
+    }
+
+    public func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+        log.error("Receiving resources is not supported")
+    }
+
+    public func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+        log.error("Receiving resources is not supported")
+    }
+}
