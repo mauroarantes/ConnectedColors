3dc3261b61e2535067980a2e9bc9a965d1b8d840
Mauro Arantes <mauro.arantes@gmail.com>
2023-10-05 11:17:05 +0100

    Changed UI

--- ConnectedColors/ContentView.swift
+++ ConnectedColors/ContentView.swift
@@ -9,10 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject var colorSession = ColorMultipeerSession()
-    
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading) {
+            Text("Connected Devices:")
+            Text(String(describing: colorSession.connectedPeers.map(\.displayName)))
+
+            Divider()
+
+            HStack {
+                ForEach(NamedColor.allCases, id: \.self) { color in
+                    Button(color.rawValue) {
+                        colorSession.send(color: color)
+                    }
+                    .padding()
+                }
+            }
+            Spacer()
+        }
+        .padding()
+        .background((colorSession.currentColor.map(\.color) ?? .clear).ignoresSafeArea())
+    }
+}
+
+extension NamedColor {
+    var color: Color {
+        switch self {
+        case .red:
+            return .red
+        case .green:
+            return .green
+        case .yellow:
+            return .yellow
+        }
     }
 }
 
